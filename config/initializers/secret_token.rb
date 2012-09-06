81b436a04d22bc11f3eb1219e94d420baf343096
TRIPODTI <relacionescomerciales@tripod.net.co>
2012-09-06 10:25:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Modulo1::Application.config.secret_token = '36de6d12d1361eb3ae0ab8b8ad492e5181bcd79a474500da7ef1788011a76bb9bd3db194769ddda998e7210e876f5ba233853056cfbb1cf8840fcf793b7246ae'
